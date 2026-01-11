7f496e95adefa23ccaa714d8b61355513d0df4b3
sharadpal8055 <sharadpal230255@gmail.com>
2026-01-11 18:14:50 +0530

    Add debugging server example

---
+++ P3-Debugging/server.js
@@ -0,0 +1,13 @@
+function calculate(width,height{
+  return width+height;
+}
+let width=10 height=5;
+if(area>100){
+  console.log("area is large")
+}
+else{
+  console.log("small")
+}
+if(width+height>100){
+  console.log("area is greator than or equal to 100")
+}
